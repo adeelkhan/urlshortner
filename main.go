617c4007fae9614b5c98bed80db4b52ecf508f72
Adeel Khan <mak000@gmail.com>
2025-01-10 12:43:27 +0500

    Fixes: *) Removing extra messages *) Generating short url that can be redirected directly. *) Removing any extra code.

--- main.go
+++ main.go
@@ -20,14 +20,6 @@ type UrlShortnerResponse struct {
 	ShortUrl string `json:"short_url"`
 }
 
-type UrlLongerRequest struct {
-	ShortUrl string `json:"short_url"`
-}
-
-type UrlLongerResponse struct {
-	LongUrl string `json:"long_url"`
-}
-
 // store for urls
 var UrlMap = map[string]string{}
 
@@ -42,11 +34,11 @@ func GetShortUrl(w http.ResponseWriter, req *http.Request) {
 	url, err := url.Parse(request.LongUrl)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		return 
+		return
 	}
 
 	query := make([]string, 0)
-	query = append(query, url.Path, url.RawQuery)
+	query = append(query, url.String())
 
 	hasher := md5.New()
 	hasher.Write([]byte(strings.Join(query, "")))
@@ -54,58 +46,45 @@ func GetShortUrl(w http.ResponseWriter, req *http.Request) {
 	shortUrl := strings.TrimSpace(sumHex)[0:URL_LENGTH]
 
 	urlResponse := &UrlShortnerResponse{
-		ShortUrl: fmt.Sprintf(`http://localhost:8090/%s`, shortUrl),
+		ShortUrl: fmt.Sprintf(`http://localhost:8090/l?q=%s`, shortUrl),
 	}
-
-	UrlMap[urlResponse.ShortUrl] = url.String()
+	UrlMap[shortUrl] = url.String()
 	responseByte, _ := json.Marshal(urlResponse)
 
 	fmt.Fprintf(w, string(responseByte))
 }
 
 func GetLongUrl(w http.ResponseWriter, req *http.Request) {
-	var request = UrlLongerRequest{}
-	err := json.NewDecoder(req.Body).Decode(&request)
+	requestURL := req.URL.RequestURI()
+	_, err := url.Parse(requestURL)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	url, err := url.Parse(request.ShortUrl)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return 
+	url_code := req.URL.Query()
+	if _, ok := url_code["q"]; !ok {
+		http.Error(w, "Invalid query", http.StatusBadRequest)
+		return
 	}
-
+	if len(url_code["q"]) == 0 {
+		http.Error(w, "Invalid query", http.StatusBadRequest)
+		return
+	}
+	hash_code := url_code["q"][0]
 	var longUrl string
-	for k,v := range UrlMap {
-		if k == url.String() {
-			longUrl = v 
+	for k, v := range UrlMap {
+		if k == hash_code {
+			longUrl = v
 			break
 		}
 	}
-
-	urlResponse := &UrlLongerResponse{
-		LongUrl: longUrl,
-	}
-
-	responseByte, _ := json.Marshal(urlResponse)
-	fmt.Fprintf(w, string(responseByte))
-}
-
-func GetAllUrls(w http.ResponseWriter, req *http.Request) {
-	for k,v := range UrlMap {
-		fmt.Println(k,":",v)
-	}
-	fmt.Fprintf(w, "longurl")
+	http.Redirect(w, req, longUrl, http.StatusPermanentRedirect)
 }
 
 func main() {
 	fmt.Println("URL Shortner Service Starting at 8090")
-
-	http.HandleFunc("/shorturl", GetShortUrl)
-	http.HandleFunc("/longurl", GetLongUrl)
-	http.HandleFunc("/allurls", GetAllUrls)
+	http.HandleFunc("/s", GetShortUrl)
+	http.HandleFunc("/l", GetLongUrl)
 	http.ListenAndServe(":8090", nil)
-
 }
